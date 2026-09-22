0a00fa7b666e3d0fada58370a77809ee41f0361c
GuilhermeO-Gomes <guilherme16062009@gmail.com>
2026-09-22 17:36:50 -0300

    Código melhorado

--- src/view/TelaAdicionarConta.java
+++ src/view/TelaAdicionarConta.java
@@ -9,6 +9,7 @@ import java.awt.Insets;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -19,19 +20,15 @@ public class TelaAdicionarConta extends JPanel {
 	    private static final long serialVersionUID = 1L;
 	    private final JTextField codigo = new JTextField(5), descricao = new JTextField(15);
 	    public TelaAdicionarConta() {
-	    	//Ver o que essas telas fazem no sentido de layout e painel
 	    	setLayout(new BorderLayout(8,8));
 	    	setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 	        montar_tela();
 	    }
 
 	    private void montar_tela() {
-	    	//Aqui começa a ter tela
-	    	//Grid é layout flexível
 	    	JPanel painel_principal = new JPanel(new GridBagLayout());
 	    	painel_principal.setBorder(BorderFactory.createTitledBorder("Nova Conta"));
 	    	GridBagConstraints organizador_principal = new GridBagConstraints();
-	    	//Cima, esquerda, baixo, direita
 	    	organizador_principal.insets = new Insets(10, 2, 10, 2);
 	    	organizador_principal.anchor = GridBagConstraints.WEST;
 	    	organizador_principal.weightx = 0;
@@ -43,100 +40,23 @@ public class TelaAdicionarConta extends JPanel {
 	    	lbl_situacao = new JLabel("Situação:"),
 	    	lbl_tipo = new JLabel("Tipo:");
 	    	String[] grupos= {"Ativos", "Passivos", "Custos", "Receita", "Despesa", "Patrimônio liquído"};
+	    	String[] naturezas = {"Devedora", "Credora"};
+	    	String[] situacoes = {"Ativo", "Inativo"};
+	    	String[] tipos = {"Analítica", "Sintética"};
 	    	JComboBox <String> grupo = new JComboBox<>(grupos);
 	    	JComboBox <String> conta_superior = new JComboBox<>(grupos);
-	    	String[] naturezas = {"Devedora", "Credora"};
 	    	JComboBox <String> natureza = new JComboBox<>(naturezas);
-	    	String[] situacoes = {"Ativo", "Inativo"};
 	    	JComboBox <String> situacao = new JComboBox<>(situacoes);
-	    	String[] tipos = {"Analítica", "Sintética"};
 	    	JComboBox <String> tipo = new JComboBox<>(tipos);
 	    	JButton salvar = new JButton("Salvar"), cancelar = new JButton("Cancelar");
 	    	
-	    	//Para o Guilherme de amanhã, tem que montar a tela, fazer o controller e melhorar o código
-	    	organizador_principal.gridx = 0;
-	    	organizador_principal.gridy = 0;
-	    	organizador_principal.weightx = 0;
-	    	organizador_principal.fill = GridBagConstraints.NONE;
-	    	painel_principal.add(lbl_codigo, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 1;
-	    	organizador_principal.gridy = 0;
-	    	organizador_principal.weightx = 1;
-	    	organizador_principal.fill = GridBagConstraints.HORIZONTAL;
-	    	painel_principal.add(codigo, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 0;
-	    	organizador_principal.gridy = 1;
-	    	organizador_principal.weightx = 0;
-	    	organizador_principal.fill = GridBagConstraints.NONE;
-	    	painel_principal.add(lbl_descricao, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 1;
-	    	organizador_principal.gridy = 1;
-	    	organizador_principal.weightx = 1;
-	    	organizador_principal.fill = GridBagConstraints.HORIZONTAL;
-	    	painel_principal.add(descricao, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 0;
-	    	organizador_principal.gridy = 2;
-	    	organizador_principal.weightx = 0;
-	    	organizador_principal.fill = GridBagConstraints.NONE;
-	    	painel_principal.add(lbl_conta_superior, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 1;
-	    	organizador_principal.gridy = 2;
-	    	organizador_principal.weightx = 1;
-	    	organizador_principal.fill = GridBagConstraints.HORIZONTAL;
-	    	painel_principal.add(conta_superior, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 0;
-	    	organizador_principal.gridy = 3;
-	    	organizador_principal.weightx = 0;
-	    	organizador_principal.fill = GridBagConstraints.NONE;
-	    	painel_principal.add(lbl_grupo, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 1;
-	    	organizador_principal.gridy = 3;
-	    	organizador_principal.weightx = 1;
-	    	organizador_principal.fill = GridBagConstraints.HORIZONTAL;
-	    	painel_principal.add(grupo, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 0;
-	    	organizador_principal.gridy = 4;
-	    	organizador_principal.weightx = 0;
-	    	organizador_principal.fill = GridBagConstraints.NONE;
-	    	painel_principal.add(lbl_natureza, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 1;
-	    	organizador_principal.gridy = 4;
-	    	organizador_principal.weightx = 1;
-	    	organizador_principal.fill = GridBagConstraints.HORIZONTAL;
-	    	painel_principal.add(natureza, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 0;
-	    	organizador_principal.gridy = 5;
-	    	organizador_principal.weightx = 0;
-	    	organizador_principal.fill = GridBagConstraints.NONE;
-	    	painel_principal.add(lbl_tipo, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 1;
-	    	organizador_principal.gridy = 5;
-	    	organizador_principal.weightx = 1;
-	    	organizador_principal.fill = GridBagConstraints.HORIZONTAL;
-	    	painel_principal.add(tipo, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 0;
-	    	organizador_principal.gridy = 6;
-	    	organizador_principal.weightx = 0;
-	    	organizador_principal.fill = GridBagConstraints.NONE;
-	    	painel_principal.add(lbl_situacao, organizador_principal);
-	    	
-	    	organizador_principal.gridx = 1;
-	    	organizador_principal.gridy = 6;
-	    	organizador_principal.weightx = 1;
-	    	organizador_principal.fill = GridBagConstraints.HORIZONTAL;
-	    	painel_principal.add(situacao, organizador_principal);
+	    	adicionar_componente(0, painel_principal, organizador_principal, lbl_codigo, codigo);
+	    	adicionar_componente(1, painel_principal, organizador_principal, lbl_descricao, descricao);
+	    	adicionar_componente(2, painel_principal, organizador_principal, lbl_conta_superior, conta_superior);
+	    	adicionar_componente(3, painel_principal, organizador_principal, lbl_grupo, grupo);
+	    	adicionar_componente(4, painel_principal, organizador_principal, lbl_natureza, natureza);
+	    	adicionar_componente(5, painel_principal, organizador_principal, lbl_tipo, tipo);
+	    	adicionar_componente(6, painel_principal, organizador_principal, lbl_situacao, situacao);
 	    	
 	    	botoes.add(cancelar);
 	    	botoes.add(salvar);
@@ -150,6 +70,27 @@ public class TelaAdicionarConta extends JPanel {
 	    	painel_principal.add(botoes, organizador_principal);
 	    	
 	        add(painel_principal, BorderLayout.NORTH);
-	    }}
+	    }
+public void adicionar_componente( 
+		int y, 
+		JPanel painel, 
+		GridBagConstraints organizador, 
+		JLabel lbl, 
+		JComponent componente  
+		) {
+	organizador.gridx = 0;
+	organizador.gridy = y;
+	organizador.weightx = 0;
+	organizador.fill = GridBagConstraints.NONE;
+	painel.add(lbl, organizador);
+	
+	organizador.gridx = 1;
+	organizador.gridy = y;
+	organizador.weightx = 1;
+	organizador.fill = GridBagConstraints.HORIZONTAL;
+	painel.add(componente, organizador);
+};	    
+}
+
 
 
--- src/view/TelaPlanoDeContas.java
+++ src/view/TelaPlanoDeContas.java
@@ -4,15 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Insets;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -46,95 +42,38 @@ public class TelaPlanoDeContas extends JPanel {
     }
 
     private void montar_tela() {
-    	//Aqui começa a ter tela
-    	//Grid é layout flexível
     	JPanel painel_principal = new JPanel(new GridBagLayout());
     	GridBagConstraints organizador_principal = new GridBagConstraints();
+    	JPanel painel_formulario = new JPanel(new GridBagLayout());
+    	painel_formulario.setBorder(BorderFactory.createTitledBorder("Filtros e Pesquisa"));
     	GridBagConstraints organizador_form = new GridBagConstraints();
-    	//Cima, esquerda, baixo, direita
     	organizador_form.insets = new Insets(4, 4, 4, 4);
     	organizador_form.anchor = GridBagConstraints.WEST;
     	organizador_form.weightx = 0;
-    	JPanel painel_formulario = new JPanel(new GridBagLayout());
     	JPanel botoes_filtro = new JPanel(new FlowLayout(FlowLayout.LEFT, 1, 5));
     	JPanel botoes = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 10));
-    	painel_formulario.setBorder(BorderFactory.createTitledBorder("Filtros e Pesquisa"));
     	JLabel lbl_codigo = new JLabel("Código:"), lbl_descricao = new JLabel("Descrição:"), 
     	lbl_grupo = new JLabel("Grupo:"), 
     	lbl_natureza = new JLabel("Natureza:"), 
     	lbl_situacao = new JLabel("Situação:");
     	String[] grupos= {"Ativos", "Passivos", "Custos", "Receita", "Despesa", "Patrimônio liquído"};
-    	JComboBox <String> grupo = new JComboBox<>(grupos);
     	String[] naturezas = {"Devedora", "Credora"};
+      	String[] situacoes = {"Ativo", "Inativo"};
+    	JComboBox <String> grupo = new JComboBox<>(grupos);
     	JComboBox <String> natureza = new JComboBox<>(naturezas);
-    	String[] situacoes = {"Ativo", "Inativo"};
     	JComboBox <String> situacao = new JComboBox<>(situacoes);
     	JButton pesquisar = new JButton("Pesquisar conta"), mostrar_todos = new JButton("Mostrar todos"),
     	criar_conta = new JButton("Adicionar conta"), atualizar_conta = new JButton("Atualizar conta"),
     	inativar_conta = new JButton("Inativar conta");
     	
-    	
-    	organizador_form.gridx = 0;
-    	organizador_form.gridy = 1;
-    	painel_formulario.add(lbl_codigo, organizador_form);
-    	
-    	organizador_form.gridx = 1;
-    	organizador_form.gridy = 1;
-    	organizador_form.weightx = 1;
-    	organizador_form.fill = GridBagConstraints.HORIZONTAL;
-    	painel_formulario.add(codigo, organizador_form);
-    	
-    	organizador_form.gridx = 0;
-    	organizador_form.gridy = 2;
-    	organizador_form.weightx = 0;
-    	organizador_form.fill = GridBagConstraints.NONE;
-    	painel_formulario.add(lbl_descricao, organizador_form);
-    	
-    	organizador_form.gridx = 1;
-    	organizador_form.gridy = 2;
-    	organizador_form.weightx = 1;
-    	organizador_form.fill = GridBagConstraints.HORIZONTAL;
-    	painel_formulario.add(descricao, organizador_form);
-    	
-    	organizador_form.gridx = 0;
-    	organizador_form.gridy = 3;
-    	organizador_form.weightx = 0;
-    	organizador_form.fill = GridBagConstraints.NONE;
-    	painel_formulario.add(lbl_grupo, organizador_form);
-    	
-    	organizador_form.gridx = 1;
-    	organizador_form.gridy = 3;
-    	organizador_form.weightx = 1;
-    	organizador_form.fill = GridBagConstraints.HORIZONTAL;
-    	painel_formulario.add(grupo, organizador_form);
-    	
-    	organizador_form.gridx = 0;
-    	organizador_form.gridy = 4;
-    	organizador_form.weightx = 0;
-    	organizador_form.fill = GridBagConstraints.NONE;
-    	painel_formulario.add(lbl_natureza, organizador_form);
-    	
-    	organizador_form.gridx = 1;
-    	organizador_form.gridy = 4;
-    	organizador_form.weightx = 1;
-    	organizador_form.fill = GridBagConstraints.HORIZONTAL;
-    	painel_formulario.add(natureza, organizador_form);
-    	
-    	organizador_form.gridx = 0;
-    	organizador_form.gridy = 5;
-    	organizador_form.weightx = 0;
-    	organizador_form.fill = GridBagConstraints.NONE;
-    	painel_formulario.add(lbl_situacao, organizador_form);
-    	
-    	organizador_form.gridx = 1;
-    	organizador_form.gridy = 5;
-    	organizador_form.weightx = 1;
-    	organizador_form.fill = GridBagConstraints.HORIZONTAL;
-    	painel_formulario.add(situacao, organizador_form);
+    	adicionar_componente(1, painel_formulario, organizador_form, lbl_codigo, codigo);
+    	adicionar_componente(2, painel_formulario, organizador_form, lbl_descricao, descricao);
+    	adicionar_componente(3, painel_formulario, organizador_form, lbl_grupo, grupo);
+    	adicionar_componente(4, painel_formulario, organizador_form, lbl_natureza, natureza);
+    	adicionar_componente(5, painel_formulario, organizador_form, lbl_situacao, situacao);
     	
     	botoes_filtro.add(pesquisar);
     	botoes_filtro.add(mostrar_todos);
-    	
     	
     	organizador_form.gridx = 1;
     	organizador_form.gridy = 6;
@@ -177,4 +116,24 @@ public class TelaPlanoDeContas extends JPanel {
     public JTable getTabelaPlanoDeContas() {
         return tabelaPlanoDeContas;
     }
+    
+    public void adicionar_componente( 
+    		int y, 
+    		JPanel painel, 
+    		GridBagConstraints organizador, 
+    		JLabel lbl, 
+    		JComponent componente  
+    		) {
+    	organizador.gridx = 0;
+    	organizador.gridy = y;
+    	organizador.weightx = 0;
+    	organizador.fill = GridBagConstraints.NONE;
+    	painel.add(lbl, organizador);
+    	
+    	organizador.gridx = 1;
+    	organizador.gridy = y;
+    	organizador.weightx = 1;
+    	organizador.fill = GridBagConstraints.HORIZONTAL;
+    	painel.add(componente, organizador);
+    };	    
 }
